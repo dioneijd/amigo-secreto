94b0bf2ca74f3c2ce14133e999a429db2e7fc64b
Dionei J. Delangolo <dionei.jd@gmail.com>
2021-01-30 22:23:19 -0300

    feat: validation before to create a group

--- src/routes.js
+++ src/routes.js
@@ -6,22 +6,31 @@ const groupController = require('./controllers/groupController')
 const personController = require('./controllers/personController')
 
 
-routes.get('/:id', (req, res) => {
-    res.render('ShowFriend/index.html')
-})
-
-
-routes.get('/', (req, res) => {
-    res.render('NewGroup/index.html')
-})
-routes.get('/showGroup/:id', (req, res) => {
-    res.render('ShowGroup/index.html')
-})
-
-
+routes.get('/groups', groupController.index)
 routes.get('/groups/:id', groupController.show)
 routes.post('/groups', groupController.store)
+routes.put('/groups', NotImplemented)
+routes.delete('/groups', NotImplemented)
+
+
+routes.get('/person', NotImplemented)
 routes.get('/person/:id', personController.show)
+routes.post('/person', NotImplemented)
+routes.put('/person/:id', NotImplemented)
+routes.delete('/person/:id', NotImplemented)
+
+
+routes.get('/p', NotImplemented)
+routes.get('/p/:id', personController.show)
+routes.post('/p', NotImplemented)
+routes.put('/p/:id', NotImplemented)
+routes.delete('/p/:id', NotImplemented)
+
+
+function NotImplemented(req, res){
+    return res.status(501).json({})
+}
+
 
 
 module.exports = routes
